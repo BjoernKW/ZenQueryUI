535f915b9c418f86993e4a22e612cef3106aa59e
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-15 19:15:48 +0200

    API

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,32 +1,29 @@
 'use strict';
 
+var zenQueryAPIRoot = 'http://localhost:8080';
 var zenQueryServices = angular.module('zenQueryServices', ['ngResource']);
- 
-zenQueryServices.factory('DatabaseConnection', ['$resource',
-	function($resource) {
-		var zenQueryAPIRoot = 'http://localhost:8080';
 
-		return $resource(zenQueryAPIRoot + '/api/v1/databaseConnections/:databaseConnectionId',
-			{ },
-			{
-				findAll: {
-					method: 'GET',
-					params: { databaseConnectionId: 'findAll' },
-					isArray: true
-				},
-				get: {
-					method: 'GET'
-				},
-				create: {
-					method: 'POST'
-				},
-				update: {
-					method: 'PUT'
-				},
-				delete: {
-					method: 'DELETE'
-				}
+zenQueryServices.factory('DatabaseConnection', function($resource) {
+	return $resource(zenQueryAPIRoot + '/api/v1/databaseConnections/:databaseConnectionId',
+		{ },
+		{
+			findAll: {
+				method: 'GET',
+				params: { databaseConnectionId: 'findAll' },
+				isArray: true
+			},
+			get: {
+				method: 'GET'
+			},
+			create: {
+				method: 'POST'
+			},
+			update: {
+				method: 'PUT'
+			},
+			delete: {
+				method: 'DELETE'
 			}
-		);
-	}
-]);
+		}
+	);
+});
