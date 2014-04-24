d7add81178dd598791984be63eb6623188023388
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-25 01:19:19 +0200

    Testing

--- karma.conf.js
+++ karma.conf.js
@@ -9,11 +9,6 @@ module.exports = function(config) {
     // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
 
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-jasmine'
-    ],
-
     // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
@@ -53,7 +48,11 @@ module.exports = function(config) {
     // - IE (only Windows)
     browsers: ['Chrome'],
 
-
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
+    
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false
